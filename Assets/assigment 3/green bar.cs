f3cc0341962055cb4ad5aa211c210a6810a6f434
MINIS-PC\miniw <miniwolfe2@gmail.com>
2025-04-15 19:43:37 -0400

    creating game input with slider

--- Assets/assigment 3/green bar.cs
+++ Assets/assigment 3/green bar.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class greenbar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Slider slider;
+    public float currentValue = 0f;
+    public int dropSpeed = 3;
 
     // Update is called once per frame
     void Update()
     {
-        
+        //creating movement for the slider, allowing the player to move the "green bar" up based on space bar clicks
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+        currentValue += 1;
+        Debug.Log(currentValue);
+        }
+       //making "green bar" go down when space bar isnt pressed 
+         currentValue = currentValue - dropSpeed* Time.deltaTime;
+         // make sure the bar moves with the space bar being pressed
+         slider.value = currentValue%slider.maxValue;
+
     }
 }
